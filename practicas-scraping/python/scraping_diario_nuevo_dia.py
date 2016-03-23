d63492b31ed5f5fa34726d5ed1ccdc22fd2fa385
Israel Lugo <hostelixisrael@gmail.com>
2016-03-23 13:39:25 -0430

    [MOD] se corrigio el error que tenian los thread, ya ejecuta el scraping en paralelo

--- practicas-scraping/python/scraping_diario_nuevo_dia.py
+++ practicas-scraping/python/scraping_diario_nuevo_dia.py
@@ -9,57 +9,56 @@ import sys
 import threading
 import logging
 
+candado_write = threading.Lock()
+
 python_version = sys.version_info.major
 
 if python_version == 2:
-	from urlparse import urljoin
+    from urlparse import urljoin
 else:
-	from urllib.parse import urljoin
-
-mutex = threading.Lock()
+    from urllib.parse import urljoin
 
 ## Configuracion para el logging de los hilos ###
-#logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] - %(threadName)-10s : %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] - %(threadName)-10s : %(message)s')
 
-def procesar_modulo(_url_main, _url_modulo):
-	
-	peticion_tmp = requests.get(urljoin(_url_main,_url_modulo['url']))
-	
-	if peticion_tmp.status_code == 200:
-		
-		scrapy_html = ScrapySoup(peticion_tmp.text,'lxml')			
-		
-		contenedor_post = scrapy_html.find('ul',{'class':'blog big'})
+class HiloScrapy(threading.Thread):
+    def __init__(self, _url_main, _url_modulo):
+        threading.Thread.__init__(self)
+        self.url_main = _url_main
+        self.url_modulo = _url_modulo
 
-		noticias = contenedor_post.find_all('li',{'class':'post'})
+    def run(self):
+        peticion_tmp = requests.get(urljoin(self.url_main,self.url_modulo['url']))
+        if peticion_tmp.status_code == 200:
+            scrapy_html = ScrapySoup(peticion_tmp.text,'lxml')
+            contenedor_post = scrapy_html.find('ul',{'class':'blog big'})
+            noticias = contenedor_post.find_all('li',{'class':'post'})
+            noticias_JSON = []
 
-		noticias_JSON = []
+            for noticia in noticias:
+                imagen_noticia = noticia.find('img').get('src')
+                contenido_noticia = noticia.find('div',{'class':'post_content'})
+                link_noticia_obj = contenido_noticia.find('a')
+                link = link_noticia_obj.get('href')
+                titulo = link_noticia_obj.getText()
+                fecha_noticia = contenido_noticia.find('li',{'class':'date'})
 
-		for noticia in noticias:
-			imagen_noticia = noticia.find('img').get('src')
-			contenido_noticia = noticia.find('div',{'class':'post_content'})
-			link_noticia_obj = contenido_noticia.find('a')
-			link = link_noticia_obj.get('href')
-			titulo = link_noticia_obj.getText()
-			fecha_noticia = contenido_noticia.find('li',{'class':'date'})
+                noticias_JSON.append({
+                    'imagen': imagen_noticia,
+                    'link': urljoin(self.url_main,link),
+                    'titulo': titulo,
+                    'fecha': fecha_noticia.getText().strip()
+                })
+                
+            text_json = json.dumps(noticias_JSON, sort_keys=True,indent=4, separators=(',', ': '))
+            name_file = self.url_modulo['titulo']+'.json'
 
-			noticias_JSON.append({
-				'imagen': imagen_noticia,
-				'link': urljoin(_url_main,link),
-				'titulo': titulo,
-				'fecha': fecha_noticia.getText().strip()
-			})
+            fichero = open(name_file, 'w')
+            fichero.write(text_json)
+            fichero.close()
+        else:
+            print("Error http %d"%peticion_tmp.status_code)
 
-		text_json = json.dumps(noticias_JSON, sort_keys=True,indent=4, separators=(',', ': '))
-		
-		
-		name_file = url_modulo['titulo']+'.json'
-		fichero = open(name_file, 'w')
-		fichero.write(text_json)
-		fichero.close()
-
-	else:
-		print("Error http %d"%peticion_tmp.status_code)
 
 URL_PRINCIPAL = "http://nuevodia.com.ve/"
 
@@ -68,25 +67,21 @@ URLS_CATEGORIAS = []
 PETICION = requests.get(URL_PRINCIPAL)
 
 if PETICION.status_code == 200:
-	
-	scrapy_html = ScrapySoup(PETICION.text,'lxml')
+    
+    scrapy_html = ScrapySoup(PETICION.text,'lxml')
 
-	elementos = scrapy_html.find_all('li',{'class':'submenu'})
+    elementos = scrapy_html.find_all('li',{'class':'submenu'})
 
-	modulos = elementos[1].ul.find_all('a')
+    modulos = elementos[1].ul.find_all('a')
 
-	URLS_CATEGORIAS =  map(lambda elem: {
-		'url': elem.get('href'), 
-		'titulo': elem.get('title')
-	}, modulos)
-
-	hilos = list()
-	
-	for url_modulo in URLS_CATEGORIAS:
-		hilo = threading.Thread(target=procesar_modulo, args=(URL_PRINCIPAL,url_modulo))
-		hilos.append(hilo)
-		hilo.start()
-		hilo.join()
+    URLS_CATEGORIAS =  map(lambda elem: {
+        'url': elem.get('href'), 
+        'titulo': elem.get('title')
+    }, modulos)
+    
+    for url_modulo in URLS_CATEGORIAS:
+        hilo = HiloScrapy(URL_PRINCIPAL,url_modulo)
+        hilo.start()
 
 else:
-	print("Error http %d"%PETICION.status_code)
+    print("Error http %d"%PETICION.status_code)
